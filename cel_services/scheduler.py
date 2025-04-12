ab31dcbec1c14d35ded6d930a64bdf4cd323f4b6
Andrew Jones <aljones15@gmail.com>
2025-04-12 12:09:48 -0400

    Start scheduler

--- cel_services/scheduler.py
+++ cel_services/scheduler.py
@@ -1,9 +1,25 @@
 import psutil, time, subprocess
+from database import ScheduledEvent
+from datetime import datetime
 
 # https://stackoverflow.com/questions/1359383/run-a-process-and-kill-it-if-it-doesnt-end-within-one-hour
 '''
 1. query for tasks occuring during this interval
 2. spawn subprocesses for each event
 '''
+dayNum = {
+  0:"Mon",
+  1:"Tues",
+  2:"Wed",
+  3:"Thurs",
+  4:"Fri",
+  5:"Sat",
+  6:"Sun"
+}
+
+def get_repeats(weekday):
+    dayName = dayNum[weekday]
+
 def poll():
-    print("poll")
+    now = datetime.now()
+    weekday = now.weekday()
