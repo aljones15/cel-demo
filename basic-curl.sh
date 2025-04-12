0c3acad412dd7d1c0e99103548f39d1a3934d0ea
Andrew Jones <aljones15@gmail.com>
2025-04-12 12:09:35 -0400

    Change repeats.

--- basic-curl.sh
+++ basic-curl.sh
@@ -1,8 +1,8 @@
-curl -X POST http://127.0.0.1:8000/schedules -H 'Content-Type: application/json' -d '{"event_id":"urn:schedule:example","start_date_time":"2025-04-11T22:36:24.681Z","duration": 180000,"name":"Mow The Lawn","repeat":"Weekly-Thursday"}'
+curl -X POST http://127.0.0.1:8000/schedules -H 'Content-Type: application/json' -d '{"event_id":"urn:schedule:example","start_date_time":"2025-04-11T22:36:24.681Z","duration": 180000,"name":"Mow The Lawn","repeat":"Thurs"}'
 
 curl http://127.0.0.1:8000/schedules?id="urn:schedule:example"
 
-curl -X PUT http://127.0.0.1:8000/schedules -H 'Content-Type: application/json' -d '{"event_id":"urn:schedule:example","start_date_time":"2025-04-11T22:36:24.681Z","duration":180000,"name":"Mow The Lawn 2","repeat":"Weekly-Monday"}'
+curl -X PUT http://127.0.0.1:8000/schedules -H 'Content-Type: application/json' -d '{"event_id":"urn:schedule:example","start_date_time":"2025-04-11T22:36:24.681Z","duration":180000,"name":"Mow The Lawn 2","repeat":"Mon,Fri"}'
 
 curl -X DELETE http://127.0.0.1:8000/schedules?id="urn:schedule:example"
 
