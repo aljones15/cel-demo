7a08505533f09610d96a800274964298db81610f
Andrew Jones <aljones15@gmail.com>
2025-04-11 20:43:41 -0400

    Use BaseModel

--- database.py
+++ database.py
@@ -2,15 +2,17 @@ from peewee import *
 
 db = SqliteDatabase(":memory:")
 
-class ScheduledEvent(Model):
-    event_id: TextField(unique=True, index=True)
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class ScheduledEvent(BaseModel):
+    event_id: CharField(unique=True, index=True)
     state_date_time: DateTimeField(unique=True, index=True)
     duration: TimestampField()
     name: CharField()
     repeat: CharField()
     last_run: DateTimeField()
-    class Meta:
-        database = db #use cel db
         
 def init_db():
     db.connect()
